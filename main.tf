cdfc0f578fc75e270690c127164a579bad286fe8
ajayvimal <57667382+ajayvimal@users.noreply.github.com>
2026-02-24 10:07:20 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,27 @@
+terraform {
+  required_version = ">= 1.5.0"
+
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0"
+    }
+  }
+
+  cloud {
+    organization = "YOUR_TFC_ORG_NAME"
+
+    workspaces {
+      name = "azure-rg-workspace"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = var.resource_group_name
+  location = var.location
+}
